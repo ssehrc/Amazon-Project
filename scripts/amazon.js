ba6c443481ab90747062d2e03f3731fbccace25a
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-16 13:04:11 -0400

    Adds more than 1 item into cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id} js-quantity-selector">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -71,19 +71,25 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             }
         });
 
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+        const quantity = Number(quantitySelector.value);
+
         if(matchingItem){
-            matchingItem.quantity += 1;
+            matchingItem.quantity += quantity;
         }else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: quantity
             });
         }
         let cartQuantity = 0;
         cart.forEach((item) => {
-          cartQuantity += item.quantity
+          cartQuantity += item.quantity;
         });
+       
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        
         
     });
 });
