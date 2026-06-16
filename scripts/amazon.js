b154afc77de969ac73614110f836f7b346f22f61
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-16 12:28:38 -0400

    commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,12 +44,12 @@ products.forEach((product) => {
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
-            <img src="images/icons/checkmark.png"
-            data-product-name="${product.name}">
+            <img src="images/icons/checkmark.png">
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
