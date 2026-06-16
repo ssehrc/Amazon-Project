b55e31c506d85fde1d3a325945e7660d3ddf329e
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-16 12:35:26 -0400

    commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,7 +49,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -63,10 +63,10 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         //data attribute
-        const productName = button.dataset.productName;
+        const productId = button.dataset.productId;
         let matchingItem;
         cart.forEach((item) => {
-            if(productName === item.productName){
+            if(productId === item.productId){
                 matchingItem = item;
             }
         });
@@ -75,7 +75,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             matchingItem.quantity += 1;
         }else{
             cart.push({
-                productName: productName,
+                productId: productId,
                 quantity: 1
             });
         }
